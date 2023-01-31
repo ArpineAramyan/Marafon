0f726b0d15c993a8cc79c1f92d49916e4a9541f8
ArpineAramyan <93595937+ArpineAramyan@users.noreply.github.com>
2023-01-31 16:14:11 +0400

    added is_prime

--- marafon.py
+++ marafon.py
@@ -1,3 +1,6 @@
+import math
+
+
 def bank(a, years):
     for i in range(years):
         a = a + a * 0.1
@@ -8,3 +11,14 @@ def palindrome(str1):
     if str1 == str1[::-1]:
         return True
     return False
+
+
+def is_prime(number):
+    if number == 1:
+        return True
+    i = 2
+    while i <= math.sqrt(number):
+        if number % i == 0:
+            return False
+        i = i + 1
+    return True
